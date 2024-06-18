fd323be3212ddddd99482078475eea07d2685ffd
dyCodes <yusufdaudu51@gmail.com>
2024-06-18 08:12:03 +0100

    feat: install react-toastify

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import '@/styles/index.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { Poppins } from 'next/font/google';
+import { ToastContainer } from 'react-toastify';
 
 const font = Poppins({ weight: ['300', '400', '500', '600', '700'], subsets: ['latin'] });
 
@@ -7,6 +9,17 @@ export default function App({ Component, pageProps }) {
 	return (
 		<div className={font.className}>
 			<Component {...pageProps} />
+			<ToastContainer
+				// position='top-right'
+				// autoClose={5000}
+				// hideProgressBar={false}
+				// newestOnTop={true}
+				closeOnClick
+				pauseOnFocusLoss={false}
+				// draggable
+				// pauseOnHover
+				// theme='light'
+			/>
 		</div>
 	);
 }
